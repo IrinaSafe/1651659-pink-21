237545c0589350714f478478738fc68ae414b5b7
Irina Viktorova <irinasafe@icloud.com>
2021-01-20 12:49:38 +0300

    Предзагрузка шрифтов, js вынесен в отдельный файл, добавлены webp в каталог

---
+++ source/js/menu.js
@@ -0,0 +1,9 @@
+let button = document.querySelector('.page-header__toggle');
+let nav = document.querySelector('.main-nav');
+let color = document.querySelector('.page-header__head-line');
+
+button.addEventListener('click', () => {
+  button.classList.toggle('page-header__toggle--opened');
+  nav.classList.toggle('main-nav--opened');
+  color.classList.toggle('page-header__head-line--opened');
+})
