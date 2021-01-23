ae328c135307c42b2c90b58b5f0e923452ec10bd
Irina Viktorova <irinasafe@icloud.com>
2021-01-23 16:14:40 +0300

    final

--- source/js/menu.js
+++ source/js/menu.js
@@ -1,7 +1,7 @@
 let button = document.querySelector('.page-header__toggle');
 let nav = document.querySelector('.main-nav');
 let color = document.querySelector('.page-header__head-line');
-nav.classList.remove('main-nav--opened');
+// nav.classList.remove('main-nav--opened');
 
 button.addEventListener('click', () => {
   button.classList.toggle('page-header__toggle--opened');
