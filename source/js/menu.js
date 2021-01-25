41b596a499bda2da65ae42afc9730566da606e28
Irina Viktorova <irinasafe@icloud.com>
2021-01-25 20:32:02 +0300

    add

--- source/js/menu.js
+++ source/js/menu.js
@@ -1,11 +1,12 @@
 // Для реализации с js тест
-// let button = document.querySelector('.page-header__toggle');
-// let nav = document.querySelector('.main-nav');
+// let button = document.querySelector('.page-header__toggle-js');
+// let nav = document.querySelector('.main-nav__js--close');
 // let color = document.querySelector('.page-header__head-line-color');
-// nav.classList.remove('main-nav--opened page-header__toggle page-header__head-line-color');
+// nav.classList.remove('main-nav__js--close');
+// nav.classList.remove('page-header__toggle-js');
 
 // button.addEventListener('click', () => {
-//   button.classList.toggle('page-header__toggle--opened');
-//   nav.classList.toggle('main-nav--opened');
+//   button.classList.toggle('page-header__toggle-js--opened');
+//   nav.classList.toggle('main-nav__js--opened');
 //   color.classList.toggle('page-header__head-line-color--opened');
 // })
